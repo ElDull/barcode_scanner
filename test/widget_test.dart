024165eeb84956a7278b09ca5287fdc1d85e2d01
5wixy <104378437+5wixy@users.noreply.github.com>
2022-06-18 21:48:58 +0300

    Added Transfering to new route after scanning + prompts

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:scanner_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ScannerApp());
+    await tester.pumpWidget(ScannerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
